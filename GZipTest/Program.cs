cfc1dff9d42c63fda93a7af66eb0368e244705ab
Rashid <dancing.sphinx@gmail.com>
2019-04-29 12:22:30 +0300

    version 1.0

---
+++ GZipTest/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GZipTest
+{
+    class Program
+    {
+        static FileStream inFile, outFile;
+
+        static Queue<Worker> wrkQueue = new Queue<Worker>();
+        static int MAX_THREADS = Environment.ProcessorCount;
+
+        static long bytesTotal = 0;
+        static bool isPack;
+
+        static int Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Wrong number of arguments");
+                return 1;
+            }
+
+            if (args[0] == "compress")
+            {
+                isPack = true;
+            }
+            else if (args[0] == "decompress")
+            {
+                isPack = false;
+            }
+            else
+            {
+                Console.WriteLine("Bad command");
+                return 1;
+            }
+
+            if (!File.Exists(args[1]))
+            {
+                Console.WriteLine("Input doesn't exist");
+                return 1;
+            }
+
+            if (new FileInfo(args[1]).Length == 0)
+            {
+                Console.WriteLine("File is empty");
+                return 1;
+            }
+
+            try
+            {
+                inFile = new FileStream(args[1], FileMode.Open);
+                outFile = new FileStream(args[2], FileMode.Create);
+
+                while (inFile.Position < inFile.Length)
+                {
+                    wrkQueue.Enqueue(new Worker(inFile, isPack));
+
+                    if (wrkQueue.Count == MAX_THREADS)
+                    {
+                        bytesTotal += wrkQueue.Dequeue().Flush(outFile);
+                    }
+
+                    Update();
+                }
+
+                foreach (Worker worker in wrkQueue)
+                {
+                    bytesTotal += worker.Flush(outFile);
+
+                    Update();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+                return 1;
+            }
+
+            inFile.Close();
+            outFile.Close();
+
+            Console.WriteLine(", done.");
+            return 0;
+        }
+
+        static void Update()
+        {
+            Console.Write("\r{0}: {1}% complete", isPack ? "Packing" : "Unpacking", bytesTotal * 100 / inFile.Length);
+        }
+    }
+}
---
+++ GZipTest/Worker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+
+namespace GZipTest
+{
+    class Worker
+    {
+        Thread wrkThread;
+        Exception wrkEx;
+
+        byte[] inBuffer, outBuffer;
+        int bytesRead;
+        int bytesUnpacked;
+        int outBufLength;
+
+        MemoryStream memStream = new MemoryStream();
+
+        bool isPack;
+
+        public Worker(Stream input, bool isPack)
+        {
+            this.isPack = isPack;
+
+            if (isPack)
+            {
+                int bufLength = 1048576;
+
+                inBuffer = new byte[bufLength];
+                bytesRead = input.Read(inBuffer, 0, bufLength);
+
+                wrkThread = new Thread(Pack);
+            }
+            else
+            {
+                int blockLength, hdrLength = 8;
+
+                inBuffer = new byte[hdrLength];
+                input.Read(inBuffer, 0, hdrLength);
+                input.Seek(-hdrLength, SeekOrigin.Current);
+                blockLength = BitConverter.ToInt32(inBuffer, 4);
+                inBuffer = new byte[blockLength];
+                bytesRead = input.Read(inBuffer, 0, blockLength);
+                outBufLength = BitConverter.ToInt32(inBuffer, inBuffer.Length - 4);
+                outBuffer = new byte[outBufLength];
+
+                wrkThread = new Thread(Unpack);
+            }
+
+            wrkThread.Start();
+        }
+
+        public int Flush(Stream output)
+        {
+            wrkThread.Join();
+
+            if (wrkEx != null)
+                throw wrkEx;
+
+            if (isPack)
+                memStream.CopyTo(output);
+            else
+                output.Write(outBuffer, 0, bytesUnpacked);
+
+            return bytesRead;
+        }
+
+        private void Pack()
+        {
+            try
+            {
+                using (GZipStream packStream = new GZipStream(memStream, CompressionMode.Compress, true))
+                {
+                    packStream.Write(inBuffer, 0, bytesRead);
+                }
+                memStream.Position = 4;
+                memStream.Write(BitConverter.GetBytes((int)memStream.Length), 0, 4);
+                memStream.Position = 0;
+            }
+            catch (Exception e)
+            {
+                wrkEx = e;
+            }
+        }
+
+        private void Unpack()
+        {
+            try
+            {
+                using (Stream stream = new MemoryStream(inBuffer))
+                using (GZipStream unpackStream = new GZipStream(stream, CompressionMode.Decompress, true))
+                {
+                    bytesUnpacked = unpackStream.Read(outBuffer, 0, outBufLength);
+                }
+            }
+            catch (Exception e)
+            {
+                wrkEx = e;
+            }
+        }
+    }
+}
